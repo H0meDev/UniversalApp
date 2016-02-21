dd78ca3ce44e71266602337f6dcc5ab05225b0b6
Charles Leo <362397585@qq.com>
2016-02-21 22:39:55 +0800

    Upload current code

--- UFoundation/UFoundation/UIFoundation/UNavigationController.m
+++ UFoundation/UFoundation/UIFoundation/UNavigationController.m
@@ -82,8 +82,6 @@
     _transformRate = (systemVersionFloat() >= 7.0)?0.35:1.0;
     
     UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc]init];
-    panGesture.maximumNumberOfTouches = 1;
-    panGesture.minimumNumberOfTouches = 1;
     panGesture.delegate = self;
     [panGesture addTarget:self action:@selector(panAction:)];
     [self.view addGestureRecognizer:panGesture];
